0026ec0e676c9e6f3647b2ce4c79a5b7adffad81
n3m3sis42 <n3m3sis42@gmail.com>
2018-06-05 17:39:27 -0400

    fix API keys

---
+++ src/config/dev.js
@@ -0,0 +1,4 @@
+export const keys = {
+  newsApi: '225d91512e644a669cff6f0e5078a05e'
+  // newsApi:'9bf65e677f9343f38858640dadd57da2'
+};
--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { merge } from 'lodash';
-import { keys } from '../config/prod';
+import { keys } from '../config/keys';
 
 const BASE_URL = 'https://newsapi.org/v2';
 const API_KEY = keys.newsApi;
